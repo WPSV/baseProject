4f3ab52963736d40aa2ca12e97bb135974c99f13
William Pedro Silva Vieira <71900080@aluno.faculdadecotemig.br>
2024-06-08 09:45:23 -0300

    Estrutura base do projeto

---
+++ src/components/Button.tsx
@@ -0,0 +1,20 @@
+import { ReactNode } from "react";
+import { Button as NextBtn } from "@nextui-org/button";
+
+type ButtonProps = {
+    width: string;
+    height: string;
+    color: string;
+    fontSize?: string;
+    backgroundColor: string;
+    border?: string;
+    children: ReactNode;
+}
+
+export default function Button({ width, height, color, fontSize, backgroundColor, border, children } : ButtonProps) {
+    return (
+        <NextBtn className={`${width} ${height} ${color} ${fontSize} ${backgroundColor} ${border} rounded-btn`}>
+            { children }
+        </NextBtn>
+    );
+}
