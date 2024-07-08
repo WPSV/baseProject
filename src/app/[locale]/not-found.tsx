79ac617b8264b45433911a37dbdc3881f8d0578b
Daniel Fonseca <daniel.fonseca@dgtallab.com.br>
2024-07-08 11:34:33 +0100

    fix: Correção tags not found

--- src/app/[locale]/not-found.tsx
+++ src/app/[locale]/not-found.tsx
@@ -11,9 +11,9 @@ export default function NotFoundPage() {
             <div
                 className="flex flex-col items-center">
                 <span className="text-9xl font-bold text-primary pb-5">404</span>
-                <h1 className="text-2xl text-lightPrimary">{t('message')}</h1>
+                <h1 className="text-2xl text-white">{t('message')}</h1>
                 <Button
-                    className="w-btn h-14 bg-primary border border-solid border-primary rounded-lg text-light text-sm font-bold mt-10">
+                    className="w-btn h-14 bg-buttonColor hover:bg-buttonBgHover hover:border-white border border-solid border-transparent rounded-lg text-buttonText text-sm font-bold mt-10">
                     {p('message')}
                 </Button>
             </div>
