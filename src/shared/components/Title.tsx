051c258184a942e5afb67d1cb22aa506fc91c333
Daniel Fonseca <daniel.fonseca@dgtallab.com.br>
2024-07-08 11:36:13 +0100

    fix: Adicionada tags de personalização no elemento  title

--- src/shared/components/Title.tsx
+++ src/shared/components/Title.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 export default function Title({children}: { children: ReactNode }) {
   return (
-    <h1 className="text-2xl font-bold leading-7 text-center text-darkPlus">
+    <h1 className="text-2xl font-bold leading-7 text-center text-colorTitle">
       {children}
     </h1>
   )
