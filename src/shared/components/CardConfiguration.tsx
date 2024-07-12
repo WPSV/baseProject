1a9e18631f3f60a4b6dc96f6fd60d8832b4353b8
Daniel Fonseca <daniel.fonseca@dgtallab.com.br>
2024-07-12 16:00:36 +0100

    feat: Criado o elemento card configuracao

--- src/shared/components/CardConfiguration.tsx
+++ src/shared/components/CardConfiguration.tsx
@@ -2,32 +2,32 @@
 
 import { Card, CardBody, CardHeader } from "@nextui-org/react";
 import Button from "./Button";
-import {UserCircle, Envelope, UsersThree, Database} from '@phosphor-icons/react'
+import { UserCircle, Envelope, UsersThree, Database } from '@phosphor-icons/react'
 
 export default function CardConfiguration() {
   return (
-    <Card radius="lg" className="bg-background flex items-center justify-center">
-      <CardHeader className="flex gap-3">
+    <Card radius="lg" className="bg-background flex items-center justify-center w-90 min-h-80 max-h-96">
+      <CardHeader className="flex gap-3 p-6">
         <div className="flex flex-col">
           <p className="text-md font-bold text-2xl mt-5">Configurações</p>
         </div>
       </CardHeader>
-      <CardBody className="p-4">
-        <Button className="bg-buttonConfig hover:bg-buttonConfigHover border-transparent border border-solid rounded-lg hover:text-buttonConfigTextHover text-buttonConfigText text-sm">
+      <CardBody className="p-6">
+        <Button className="flex items-center justify-start bg-buttonConfig hover:bg-buttonConfigHover border-transparent border border-solid rounded-lg hover:text-buttonConfigTextHover text-buttonConfigText text-sm">
           <UserCircle size={32} className="mr-2" />
-          <span>Perfil</span>
+          <span className="font-light">Perfil</span>
         </Button>
-        <Button className="bg-buttonConfig mt-5 hover:bg-buttonConfigHover border-transparent border border-solid rounded-lg hover:text-buttonConfigTextHover text-buttonConfigText text-sm">
+        <Button className="flex items-center justify-start bg-buttonConfig mt-5 hover:bg-buttonConfigHover border-transparent border border-solid rounded-lg hover:text-buttonConfigTextHover text-buttonConfigText text-sm">
           <Envelope size={32} className="mr-2" />
-          <span>Email de notificação</span>
+          <span className="font-light">Email de notificação</span>
         </Button>
-        <Button className="bg-buttonConfig mt-5 hover:bg-buttonConfigHover border-transparent border border-solid rounded-lg hover:text-buttonConfigTextHover text-buttonConfigText text-sm">
+        <Button className="flex items-center justify-start bg-buttonConfig mt-5 hover:bg-buttonConfigHover border-transparent border border-solid rounded-lg hover:text-buttonConfigTextHover text-buttonConfigText text-sm">
           <UsersThree size={32} className="mr-2" />
-          <span>Time</span>
+          <span className="font-light">Time</span>
         </Button>
-        <Button className="bg-buttonConfig mt-5 hover:bg-buttonConfigHover border-transparent border border-solid rounded-lg hover:text-buttonConfigTextHover text-buttonConfigText text-sm">
+        <Button className="flex items-center justify-start bg-buttonConfig mt-5 hover:bg-buttonConfigHover border-transparent border border-solid rounded-lg hover:text-buttonConfigTextHover text-buttonConfigText text-sm">
           <Database size={32} className="mr-2" />
-          <span>Plano</span>
+          <span className="font-light">Plano</span>
         </Button>
       </CardBody>
     </Card>
