c847366a6a6d56bf1310241df9864b163a384703
Daniel Fonseca <daniel.fonseca@dgtallab.com.br>
2024-07-12 18:04:50 +0100

    feat: Adicionados estilos de tags no card configuracao

--- src/shared/components/CardConfiguration.tsx
+++ src/shared/components/CardConfiguration.tsx
@@ -6,7 +6,7 @@ import { UserCircle, Envelope, UsersThree, Database } from '@phosphor-icons/reac
 
 export default function CardConfiguration() {
   return (
-    <Card radius="lg" className="bg-background flex items-center justify-center w-90 min-h-80 max-h-96">
+    <Card radius="lg" className="bg-colorCard flex items-center justify-center w-90 min-h-80 max-h-96">
       <CardHeader className="flex gap-3 p-6">
         <div className="flex flex-col">
           <p className="text-md font-bold text-2xl mt-5">Configurações</p>
@@ -14,19 +14,19 @@ export default function CardConfiguration() {
       </CardHeader>
       <CardBody className="p-6">
         <Button className="flex items-center justify-start bg-buttonConfig hover:bg-buttonConfigHover border-transparent border border-solid rounded-lg hover:text-buttonConfigTextHover text-buttonConfigText text-sm">
-          <UserCircle size={32} className="mr-2" />
+          <UserCircle size={25} className="mr-2" />
           <span className="font-light">Perfil</span>
         </Button>
         <Button className="flex items-center justify-start bg-buttonConfig mt-5 hover:bg-buttonConfigHover border-transparent border border-solid rounded-lg hover:text-buttonConfigTextHover text-buttonConfigText text-sm">
-          <Envelope size={32} className="mr-2" />
+          <Envelope size={25} className="mr-2" />
           <span className="font-light">Email de notificação</span>
         </Button>
         <Button className="flex items-center justify-start bg-buttonConfig mt-5 hover:bg-buttonConfigHover border-transparent border border-solid rounded-lg hover:text-buttonConfigTextHover text-buttonConfigText text-sm">
-          <UsersThree size={32} className="mr-2" />
+          <UsersThree size={25} className="mr-2" />
           <span className="font-light">Time</span>
         </Button>
         <Button className="flex items-center justify-start bg-buttonConfig mt-5 hover:bg-buttonConfigHover border-transparent border border-solid rounded-lg hover:text-buttonConfigTextHover text-buttonConfigText text-sm">
-          <Database size={32} className="mr-2" />
+          <Database size={25} className="mr-2" />
           <span className="font-light">Plano</span>
         </Button>
       </CardBody>
