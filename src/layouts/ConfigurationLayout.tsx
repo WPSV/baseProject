50cae9b23f368159086b18f801465b078bf2e476
Daniel Fonseca <daniel.fonseca@dgtallab.com.br>
2024-07-12 15:59:46 +0100

    fix: Correcao no layout da configuracao de layout

--- src/layouts/ConfigurationLayout.tsx
+++ src/layouts/ConfigurationLayout.tsx
@@ -2,19 +2,19 @@ import React from 'react';
 import Sidebar from '@/shared/components/Sidebar';
 import PageLocation from '@/shared/components/pagelocation';
 import CardConfiguration from '@/shared/components/CardConfiguration';
-import CardPlane from '@/shared/components/CardPlane';
+import CardProfile from '@/shared/components/CardProfile';
 
 export default function ConfigurationLayout() {
   return (
-    <div className="flex w-screen h-screen bg-darkPlus">
+    <div className="relative flex w-screen h-screen bg-background">
       <Sidebar />
       <div className="flex-grow flex flex-col p-8">
-        <div className="w-[1248px] flex flex-col gap-4 mt-10">
+        <div className="w-full flex flex-col gap-4 mt-10">
           <PageLocation />
           <div className="grid grid-cols-3 grid-rows-1 gap-4 mt-5">
             <CardConfiguration />
             <div className="col-span-2">
-              <CardPlane />
+              <CardProfile />
             </div>
           </div>
         </div>
