e6bd915a909839dd66cc015e00565ee1b7a7681e
Daniel Fonseca <daniel.fonseca@dgtallab.com.br>
2024-07-12 21:31:21 +0100

    feat: Adicionados estilos de tags no card de notificacoes

--- src/shared/components/CardNotificationEmail.tsx
+++ src/shared/components/CardNotificationEmail.tsx
@@ -9,44 +9,44 @@ export default function CardNotificationEmail() {
 
   const handleToggle = () => setChecked(!checked);
   return (
-    <Card radius="lg" className="bg-background flex items-center justify-center mb-5">
+    <Card radius="lg" className="bg-colorCard flex items-center justify-center mb-5">
       <CardHeader className="flex gap-3 p-6">
         <div className="flex flex-col">
-          <p className="text-md font-bold text-2xl mt-5">Email de nofiticação</p>
+          <p className="text-md font-bold text-2xl mt-5 text-colorTitle">Email de nofiticação</p>
         </div>
       </CardHeader>
       <CardBody className="p-4">
-        <span className="text-gray-700 p-2 font-bold">Newsletter:</span>
-        <div className="p-4 rounded-lg border border-gray-300 hover:border-gray-400 flex items-center justify-between">
-          <span className="text-gray-700">Newsletter</span>
+        <span className="p-2 font-bold text-colorTitle">Newsletter:</span>
+        <div className="p-4 rounded-lg border border-borderInput hover:border-gray-400 flex items-center justify-between">
+          <span className="text-colorTitle">Newsletter</span>
           <Switch checked={checked} onChange={handleToggle} />
         </div>
 
-        <span className="text-gray-700 p-2 font-bold mt-5">Oportunidades:</span>
-        <div className="p-4 rounded-lg border border-gray-300 hover:border-gray-400 flex items-center justify-between mb-2">
-          <span className="text-gray-700">Novas oportunidades lançadas na plataforma</span>
+        <span className="p-2 font-bold mt-5 text-colorTitle">Oportunidades:</span>
+        <div className="p-4 rounded-lg border border-borderInput hover:border-gray-400 flex items-center justify-between mb-2">
+          <span className="text-colorTitle">Novas oportunidades lançadas na plataforma</span>
           <Switch checked={checked} onChange={handleToggle} />
         </div>
 
-        <div className="p-4 rounded-lg border border-gray-300 hover:border-gray-400 flex items-center justify-between">
-          <span className="text-gray-700">E-mails com informações relevantes para você</span>
+        <div className="p-4 rounded-lg border border-borderInput hover:border-gray-400 flex items-center justify-between">
+          <span className="text-colorTitle">E-mails com informações relevantes para você</span>
           <Switch checked={checked} onChange={handleToggle} />
         </div>
 
-        <span className="text-gray-700 p-2 font-bold mt-5">Plano:</span>
-        <div className="p-4 rounded-lg border border-gray-300 hover:border-gray-400 flex items-center justify-between mb-2">
-          <span className="text-gray-700">Notificações de planos</span>
+        <span className="p-2 font-bold mt-5 text-colorTitle">Plano:</span>
+        <div className="p-4 rounded-lg border border-borderInput hover:border-gray-400 flex items-center justify-between mb-2">
+          <span className="text-colorTitle">Notificações de planos</span>
           <Switch checked={checked} onChange={handleToggle} />
         </div>
 
-        <div className="p-4 rounded-lg border border-gray-300 hover:border-gray-400 flex items-center justify-between">
-          <span className="text-gray-700">E-mails com informações relevantes para você</span>
+        <div className="p-4 rounded-lg border border-borderInput hover:border-gray-400 flex items-center justify-between">
+          <span className="text-colorTitle">E-mails com informações relevantes para você</span>
           <Switch checked={checked} onChange={handleToggle} />
         </div>
 
-        <span className="text-gray-700 p-2 font-bold mt-5">Aplicações:</span>
-        <div className="p-4 rounded-lg border border-gray-300 hover:border-gray-400 flex items-center justify-between mb-5">
-          <span className="text-gray-700">Um lembrete para aplicativos que foram iniciados, mas não concluídos</span>
+        <span className="p-2 font-bold mt-5 text-colorTitle">Aplicações:</span>
+        <div className="p-4 rounded-lg border border-borderInput hover:border-gray-400 flex items-center justify-between mb-5">
+          <span className="text-colorTitle">Um lembrete para aplicativos que foram iniciados, mas não concluídos</span>
           <Switch checked={checked} onChange={handleToggle} />
         </div>
       </CardBody>
