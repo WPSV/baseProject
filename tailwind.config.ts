238051c8a3b3cca0810e26572b4c6bfd35161a98
William Pedro Silva Vieira <71900080@aluno.faculdadecotemig.br>
2024-07-18 11:33:20 -0300

    Ajustes tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,6 +4,8 @@ module.exports = {
   darkMode: "class",
   important: true,
   content: [
+    './src/**/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/shared/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
